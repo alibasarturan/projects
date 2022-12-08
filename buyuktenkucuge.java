20fd88319dd37a40d631a9a0311c65daace1a5ce
alibasarturan <118301472+alibasarturan@users.noreply.github.com>
2022-12-08 23:21:45 +0300

    www.patika.dev

---
+++ buyuktenkucuge.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class buyuktenkucuge {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner input = new Scanner(System.in);
+        System.out.print("İlk sayıyı giriniz:");
+        a = input.nextInt();
+        System.out.print("İkinci sayıyı giriniz:");
+        b = input.nextInt();
+        System.out.print("Üçüncü sayıyı giriniz:");
+        c = input.nextInt();
+        if (a > b && a > c)
+        {
+            if (b > c) {
+                System.out.print("a>b>c");
+            }else {System.out.println("a>c>b");}
+
+
+            }
+        else if (b>a&&b>c){
+            if(a>c){
+                System.out.println("b>a>c");}
+            else {System.out.println("b > c > a");}
+        }
+        else {
+            if (a>b){
+
+                    System.out.println("c>a>b");}
+                    else {System.out.println("c>b>a");}
+
+
+                    ;
+        }
+
+    }}
